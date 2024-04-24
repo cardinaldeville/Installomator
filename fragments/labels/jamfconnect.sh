b58672a9fe3de19b38a96331d6aed5e7a472544e
Chris Abbs <chris@meissa.net>
2024-04-24 14:54:08 -0500

    Update jamfconnect.sh

--- fragments/labels/jamfconnect.sh
+++ fragments/labels/jamfconnect.sh
@@ -5,4 +5,5 @@ jamfconnect)
     appNewVersion=$(getJSONValue "$(curl -s "https://learn-be.jamf.com/api/bundlelist?name_filter.field=name&name_filter.value=jamf-connect-documentation-current")" ".bundle_list[0].title" | awk -F ' ' '{print $NF}')
     downloadURL="https://files.jamfconnect.com/JamfConnect.dmg"
     expectedTeamID="483DWKW443"
+    blockingProcesses=( "Jamf Connect" )
     ;;
